315f99db20be24b92d403c58422d2c002264a49a
Lars-Bak <larsbakker.25@gmail.com>
2026-04-03 14:06:44 +0200

    Delete RollercoastersFlask directory

--- RollercoastersFlask/extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
-
-# ---------- User is limited by 20 requests per minute to the webadres ---------- #
-
-limiter = Limiter(
-    key_func=get_remote_address,
-    default_limits=["20 per minute"]
-)
